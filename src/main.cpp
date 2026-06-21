4a27ce661dbdf9a82f0889f3221d0b0582dc29dd
nurikk <1525421+nurikk@users.noreply.github.com>
2026-06-21 09:21:49 +0100

    refactor: extract life_profile.h from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -72,31 +72,7 @@ Adafruit_LIS3DH accelerometer = Adafruit_LIS3DH();
 
 #include "life_state.h"
 #include "life_util.h"
-
-void addProfile(uint32_t &total, uint32_t &maximum, uint32_t elapsed) {
-  total += elapsed;
-  if (elapsed > maximum) {
-    maximum = elapsed;
-  }
-}
-
-uint32_t averageProfile(uint32_t total, uint16_t samples) {
-  return samples ? total / samples : 0;
-}
-
-void resetProfileCounters() {
-  profileLoopMicros = 0;
-  profileLifeMicros = 0;
-  profileAccelMicros = 0;
-  profileRenderMicros = 0;
-  profileShowMicros = 0;
-  profileLoopMaxMicros = 0;
-  profileLifeMaxMicros = 0;
-  profileRenderMaxMicros = 0;
-  profileShowMaxMicros = 0;
-  profileSamples = 0;
-  profileRenderSamples = 0;
-}
+#include "life_profile.h"
 
 uint8_t motionType() {
   return ((static_cast<uint16_t>(wrapHue(motionGlow + tiltStrength + 19)) *
@@ -1075,78 +1051,6 @@ void decayMotionEffects() {
   } else {
     motionGlow = 0;
   }
-}
-
-void reportFps() {
-  uint32_t now = millis();
-  uint32_t elapsed = now - fpsStartedAt;
-  if (elapsed < 1000) {
-    return;
-  }
-
-  uint32_t refreshCount = matrix.getFrameCount();
-  uint32_t refreshFps = (refreshCount * 1000UL) / elapsed;
-  uint32_t renderFps = (framesThisPeriod * 1000UL) / elapsed;
-  uint32_t lifeUps = (lifeStepsThisPeriod * 1000UL) / elapsed;
-
-  Serial.print("Life UPS: ");
-  Serial.print(lifeUps);
-  Serial.print(" | Render FPS: ");
-  Serial.print(renderFps);
-  Serial.print(" | Refresh FPS: ");
-  Serial.print(refreshFps);
-  Serial.print(" | live: ");
-  Serial.print(liveCells);
-  Serial.print(" | changed: ");
-  Serial.print(changedCells);
-  Serial.print(" | pixels: ");
-  Serial.print(updatedPixels);
-  Serial.print(" | events: ");
-  Serial.print(randomEventsThisPeriod);
-  Serial.print(" | motion: ");
-  Serial.print(interactionEventsThisPeriod);
-  Serial.print(" | knocks: ");
-  Serial.print(knockEventsThisPeriod);
-  Serial.print(" | burns: ");
-  Serial.print(burnEventsThisPeriod);
-  Serial.print(" | shakes: ");
-  Serial.print(shakeEventsThisPeriod);
-  Serial.print(" | tilt: ");
-  Serial.print(tiltStrength);
-  Serial.print(" | gen: ");
-  Serial.print(generation);
-  Serial.print(" | avg us loop/life/render/show/accel: ");
-  Serial.print(averageProfile(profileLoopMicros, profileSamples));
-  Serial.print('/');
-  Serial.print(averageProfile(profileLifeMicros, lifeStepsThisPeriod));
-  Serial.print('/');
-  Serial.print(averageProfile(profileRenderMicros, profileRenderSamples));
-  Serial.print('/');
-  Serial.print(averageProfile(profileShowMicros, profileRenderSamples));
-  Serial.print('/');
-  Serial.print(averageProfile(profileAccelMicros, profileSamples));
-  Serial.print(" | max us loop/life/render/show: ");
-  Serial.print(profileLoopMaxMicros);
-  Serial.print('/');
-  Serial.print(profileLifeMaxMicros);
-  Serial.print('/');
-  Serial.print(profileRenderMaxMicros);
-  Serial.print('/');
-  Serial.println(profileShowMaxMicros);
-
-#if WIFI_PORTAL_ENABLED
-  gStatRenderFps = (uint16_t)renderFps;
-  gStatLifeUps = (uint16_t)lifeUps;
-#endif
-  fpsStartedAt = now;
-  framesThisPeriod = 0;
-  lifeStepsThisPeriod = 0;
-  randomEventsThisPeriod = 0;
-  interactionEventsThisPeriod = 0;
-  knockEventsThisPeriod = 0;
-  burnEventsThisPeriod = 0;
-  shakeEventsThisPeriod = 0;
-  resetProfileCounters();
 }
 
 void setup() {
